b42e2ae0b0901b886fb22c9786e25eab91d6cd6a
u15284 <u15284@82887923-8737-45ff-9076-c468179b89ee>
2017-07-27 05:17:10 +0000

    This commit was made using a html form.

--- gettingstarted/heroku_test_runner.py
+++ gettingstarted/heroku_test_runner.py
@@ -1,30 +1,39 @@
 import os
 
+
 from django.test.runner import DiscoverRunner
+
 
 """
 WARNING:  WHEN USED INCORRECTLY THIS TEST RUNNER WILL DROP ALL TABLES IN YOUR PRODUCTION 
 DATABASE!!!
+
 
 Heroku does not give users createdb/dropdb permissions, therefore Heroku CI cannot run tests for django.
 In order to fix this, use this test runner instead which attempts to minimally override the 
 default test runner by a)  forcing keepdb=True to stop database create/drop, and b) by dropping all
 tables after a test run and resetting the database to its initial blank state.
 
+
 Usage:
+
 
 1. In your django test settings file add the following two lines to ensure that the test 
 database name is the same as the Heroku provided database name.
 
+
 DATABASES['default'] = env.db('DATABASE_URL')  # or whatever you use to load the Heroku database settings
 DATABASES['default']['TEST'] = {'NAME': DATABASES['default']['NAME']}
+
 
 2. Set the testrunner to this file
 TEST_RUNNER = 'your_modules.HerokuDiscoverRunner'
 
+
 3. Set an environment variable on heroku CI of IS_HEROKU_TEST=1 to enable this runner, otherwise
 the runner will exit as a safety measure.
 """
+
 
 class HerokuDiscoverRunner(DiscoverRunner):
     def setup_databases(self, **kwargs):
@@ -34,6 +43,7 @@ class HerokuDiscoverRunner(DiscoverRunner):
                 "This test runner will wipe all tables in the database it targets!")
         self.keepdb = True
         return super(HerokuDiscoverRunner, self).setup_databases(**kwargs)
+
 
     def _wipe_tables(self, connection):
         with connection.cursor() as cursor:
@@ -46,6 +56,7 @@ class HerokuDiscoverRunner(DiscoverRunner):
                     COMMENT ON SCHEMA public IS 'standard public schema';
                 """
             )
+
 
     def teardown_databases(self, old_config, **kwargs):
         self.keepdb = True
